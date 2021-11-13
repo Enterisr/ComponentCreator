065a91dcc5a3b57254eb3918d65a9da8651e00e3
unknown <drunktolstoy@gmail.com>
2021-11-13 14:57:55 +0200

    generic templates, yargs support

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
 #!/usr/bin/env node
 const fs = require("fs").promises;
 const path = require("path");
-const GeneratecomponentJS = require("./GeneratecomponentJS");
-const componentName = process.argv[2];
-let folderPath = process.argv[3] || "./";
-(async function CreateComponent() {
+const yargs = require("yargs/yargs")(process.argv.slice(2));
+require("dotenv").config();
+const colors = require("colors");
+const argv = yargs
+  .option("folder", {
+    default: "./",
+    alias: "D",
+    alias: "F",
+    describe:
+      "The folder you want to create the component it- By default it will try to create in CWD",
+  })
+  .option("name", {
+    alias: "n",
+    describe: "component's name",
+  })
+  .option("template", { alias: "t", describe: "template" })
+  .positional("name", { describe: "component's name" })
+  .demandOption(["name"])
+  .showHelpOnFail(true)
+  .env("componentCreator")
+  .help().argv;
+
+async function copyFile({ templateFile, templateDir, newDirPath }) {
+  const templateFilePath = path.join(templateDir, templateFile);
+  const templateFileStat = await fs.lstat(templateFilePath);
+
+  if (templateFileStat.isFile()) {
+    const newFileName = templateFile
+      .replace("__Comp__", argv.name)
+      .replace(".template", "");
+    const newLocation = path.join(newDirPath, newFileName);
+    const fileContent = await fs.readFile(templateFilePath, "utf8");
+    const newFileContent = fileContent.replaceAll("__Comp__", argv.name);
+    await fs.writeFile(newLocation, newFileContent);
+  }
+}
+async function copyTemplateFiles(templateName, destPath) {
   try {
-    console.log("creating component!");
-    const jsString = GeneratecomponentJS(componentName);
+    const newDirPath = path.join(destPath, argv.name);
+    await fs.mkdir(newDirPath, { recursive: true });
+
+    const templateDir = path.join("./templates", templateName);
+    const templateFiles = await fs.readdir(templateDir);
+    templateFiles.forEach(
+      async (templateFile) =>
+        await copyFile({ templateFile, templateDir, newDirPath })
+    );
+  } catch (ex) {
+    console.error(ex);
+  }
+}
+
+(async function CreateComponent() {
+  console.log("\n" + "chosen template: " + argv.template + "\n");
+
+  const componentName = argv.name;
+  const folderPath = argv.folder;
+  try {
     const folderName = path.join(folderPath, componentName);
-    console.log(`in path: ${folderName}`);
-    const jsFile = path.join(folderName, componentName + ".js");
-    const scssFile = path.join(folderName, componentName + ".scss");
-    await fs.mkdir(folderName, { recursive: true });
-    await fs.writeFile(jsFile, jsString);
-    await fs.writeFile(scssFile, "");
-    console.log("finished creating component...");
+    console.log(`Creating ${componentName} in path: ${folderName} \n`);
+    await copyTemplateFiles(argv.template, folderPath);
+    console.log("component created successfully! \n".bold);
   } catch (ex) {
     console.error("cant create component: " + ex);
   }
